b38b9d2f34c740d6fc6818e9992ef7eabc2f7a0f
kac <andriikushlyk@bitbucket.org>
2020-06-05 01:56:20 +0300

    Modified file php

--- index.php
+++ index.php
@@ -3,5 +3,7 @@ echo "Hello World<br/>";
 echo "first deploy via awscli<br/>";
 echo "second deploy via awscli<br/>";
 echo "deploy via function<br/>";
+echo "deploy via vscodium";
 phpinfo();
 ?>
+cd
